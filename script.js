b5b1f7d3c2f726af61b500a29f4ccd0319979736
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 14:07:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Good Luck 💪🏾
+function reverseWords(sentence) {
+  let words = sentence.split(" ");
+  words.reverse();
+  let reversedSentence = words.join(" ");
+  return reversedSentence;
+}
+console.log(reverseWords("fikrcamp bootcamp")); // Output: "bootcamp fikrcamp"
+console.log(reverseWords("hello there!")); // Output: "there! hello"
+console.log(reverseWords("welcome to my city")); // Output: "city my to welcome"
